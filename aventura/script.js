a8c463ee5f20d901a07e1c28cdefec7aeaa48062
jgod12508 <00001120015649sp@al.educacao.sp.gov.br>
2024-10-09 08:00:18 -0300

    Add files via upload

---
+++ aventura/script.js
@@ -0,0 +1,13 @@
+//João Gabriel de Oliveira Duarte
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');   
+    
+    })
+})
